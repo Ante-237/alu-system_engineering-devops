1ffab79e4f396241c6168a56d915d514bd3295fa
Ante-237 <akumawahanye@gmail.com>
2022-09-13 20:09:47 +0200

    added target path

--- ssh/100-puppet_ssh_config.pp
+++ ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,7 @@
 # changes to configuration file
 ::ssh::client::config::user { 'ubuntu':
   ensure => present,
+  target => '/etc/ssh/ssh_config',
   options => {
     'PasswordAuthentication' => 'no'
     'IdentifyFile' => '~/.ssh/school'
