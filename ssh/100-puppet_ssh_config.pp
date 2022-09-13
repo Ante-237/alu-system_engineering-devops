4930b94d37d09f2f503d2b6aed5f1fd6107e980c
Ante-237 <akumawahanye@gmail.com>
2022-09-13 20:20:02 +0200

    some fix

--- ssh/100-puppet_ssh_config.pp
+++ ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,6 @@
 # changes to configuration file
-::ssh::client::config::user { 'ubuntu':
-  ensure => present,
-  target => '/etc/ssh/ssh_config',
-  options => {
-    'PasswordAuthentication' => 'no'
-    'IdentifyFile' => '~/.ssh/school'
+file  { '/etc/ssh/ssh_config':
+    PasswordAuthentication => 'no',
+    IdentifyFile => '~/.ssh/school',
   }
 }
